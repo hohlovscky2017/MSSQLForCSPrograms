9a5d706bcf1dd5cbaee0b6ef1681af02020542c4
Дмитрий Хохловский <hohlovskiido@gmail.com>
2021-09-04 15:06:54 +0300

    Настроил подключение к базе данных

--- MSSQLForCSPrograms/Form1.cs
+++ MSSQLForCSPrograms/Form1.cs
@@ -7,14 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace MSSQLForCSPrograms
 {
     public partial class Form1 : Form
     {
+        private SqlConnection sqlConnection = null;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["SQL_DB"].ConnectionString);
+             
+            sqlConnection.Open();                       
         }
     }
 }
